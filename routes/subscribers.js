43c6117981d7edb52cff25774dba4211eae4e947
Cristiano Caon <cristiano.caon@hotmail.com>
2020-11-19 10:29:12 -0600

    feat: add subscribers routes boilerplate

---
+++ routes/subscribers.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+// Get all subscribers
+router.get('/', (req, res) => {
+
+})
+
+// Get a specific subscriber
+router.get('/:id', (req, res) => {
+  
+})
+
+// Create one subscriber
+router.post('/', (req, res) => {
+  
+})
+
+// Update specific subscriber
+router.patch('/:id', (req, res) => {
+  
+})
+
+// Delete specific subscriber
+router.delete('/:id', (req, res) => {
+  
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const subscribersRouter = require('./routes/subscribers')
 
 const app = express()
 
 app.use(express.json())
-
+app.use('/subscribers', subscribersRouter)
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
