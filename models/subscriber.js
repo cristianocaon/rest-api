451d574f993b035ae5ec57bfea1282ab75d7a4f0
Cristiano Caon <cristiano.caon@hotmail.com>
2020-11-19 18:48:50 -0600

    feat: add Subscriber model

---
+++ models/subscriber.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const subscriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  subscribedChannel: {
+    type: String,
+    require: true
+  },
+  subscribeDate: {
+    type: Date,
+    require: true,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Subscriber', subscriberSchema)
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
 
 // Get all subscribers
 router.get('/', (req, res) => {
-
+  res.send('Hello World!')
 })
 
 // Get a specific subscriber
